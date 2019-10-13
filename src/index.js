f35d8a4b60b63fbab1afac786d6c332871ac7f20
tyunegov <tyunegov.dy@mail.ru>
2019-10-13 09:20:52 +0500

    answer

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+    focus = 'family', 
+    knowsProgramming = true,
+    config = {family: 4}
+    ) {
+      if (knowsProgramming == true) {
+        return Math.ceil(800 / config[focus]);
+      } else {
+        return Math.ceil(1300 / config[focus]);	
+      }
+  };
+  
